e70d4fd88fabf18cffe513cc7577f6ffcb7a731e
ashish <166103540+ashishjaiswal24@users.noreply.github.com>
2024-07-22 17:14:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('welecom welcome wleocmee');});
+  res.send('hello ashish welcome !!! you have done it');});
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
